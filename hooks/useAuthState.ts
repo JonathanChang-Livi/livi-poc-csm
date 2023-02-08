d96ede4eee15bed5541c0c497dff8c417f64b17e
jonathanchang <cpk826@gmail.com>
2023-02-09 01:21:49 +0800

    Update useAuthState.ts

--- hooks/useAuthState.ts
+++ hooks/useAuthState.ts
@@ -1,12 +1,15 @@
 import { getCookie, setCookie } from "cookies-next"
+import { useState } from "react"
 import { create } from "zustand"
 import { getStringOrUndefined, IShareState } from "../components/interface"
 
 export const key = 'auth-token'
 
-const useAuthState = create<IShareState<string | undefined>>()(() => ({
-    current: getStringOrUndefined(getCookie(key)),
-    update: (value) => setCookie(key, value)
-}))
+const useAuthState = useState<string | undefined>(undefined)
+
+// const useAuthState = create<IShareState<string | undefined>>()(() => ({
+//     current: getStringOrUndefined(getCookie(key)),
+//     update: (value) => setCookie(key, value)
+// }))
 
 export default useAuthState
